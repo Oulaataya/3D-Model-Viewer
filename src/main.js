3906ecbc5eb6526f320cc5090c1e52c76ea5d9cc
Oulaataya <158582733+Oulaataya@users.noreply.github.com>
2026-07-08 00:18:23 +0300

    ..

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ document.body.appendChild(renderer.domElement);
 
 // 4. Cube
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00fffc });
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
