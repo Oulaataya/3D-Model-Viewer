6c7b96ba5e777ed392257207ec3427aa2c0f7e10
Oulaataya <158582733+Oulaataya@users.noreply.github.com>
2026-07-08 00:11:29 +0300

    added three.js scene

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ document.body.appendChild(renderer.domElement);
 
 // 4. Cube
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ffcc });
+const material = new THREE.MeshBasicMaterial({ color: 0x00fffc });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
